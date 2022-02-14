1b6de25384f366cba56b2da8c8696498f4a18b9e
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-14 06:58:36 +0530

    Added max method with generics

---
+++ FindMax.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.generics;
+
+import com.bridgelabz.generics.exception.FindMaxException;
+
+import java.util.Arrays;
+
+/*
+ * Generic Class To Find Max Values
+ */
+public class FindMax<E extends Comparable<E>> {
+    E[] values;
+
+    @SafeVarargs
+    public FindMax(E... values) {
+        this.values = values;
+    }
+
+    public E findMaxValue() throws FindMaxException {
+        if (values.length == 0) {
+            throw new FindMaxException("No Elements to Compare");
+        } else {
+            Arrays.sort(values);
+            printMax(values[values.length - 1]);
+            return values[values.length - 1];
+        }
+    }
+
+    /*
+     *Method to print Max Value
+     * @param is Generic Value
+     */
+    public void printMax(E value) {
+        System.out.println("Max Value = " + value);
+    }
+}
+
+
+
